e2ec35d2fd05b180a16acf37e759b00bd589e77c
eduardo24b <eduardobarrio27@gmail.com>
2023-02-23 19:46:33 -0430

    Actualizado

--- Ejercicios de Eduardo/Python/Cual es el mayor de una cantidad indefinidad de numeros.py
+++ Ejercicios de Eduardo/Python/Cual es el mayor de una cantidad indefinidad de numeros.py
@@ -1,9 +1,8 @@
 
-print("### Algoritmo para ver el mayor de una cantida indefinidad de enteros ###")
-print("Para dejar de ingresar numeros precione ¨t¨")
+print("### Algoritmo para ver el mayor y el menor de ### \n  ### una cantida indefinidad de enteros ###")
+print("Para dejar de ingresar numeros precione ¨t¨\n")
 mayor = 0
 mayor = int(mayor)
-
 lista = []
 a = True
 while a == True:
@@ -12,11 +11,19 @@ while a == True:
     if numero == "t":
         a = False
 lista.pop()
+menor = lista[0]
+menor = int(menor)
 for num in lista:
     num = int(num)
     if num > mayor:
         mayor = num
+for nam in lista:
+    nam = int(nam)
+    if nam < menor:
+        menor = nam
+
+print(f"El menor es : {menor}")        
 print(f"El mayor es : {mayor}")  
 print(lista)
-exit = input("precione enter para terminar el algoritmo")
+exit = input("precione enter para terminar el algoritmo : ")
         
